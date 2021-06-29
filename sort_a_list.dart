75f59f8273cce596429fada05875d763353630d0
Ahelee Bhattacharya <46274158+AheleeBhattacharya@users.noreply.github.com>
2021-06-29 20:10:37 +0530

    Create sort_a_list.dart

---
+++ sort_a_list.dart
@@ -0,0 +1,12 @@
+// Main fumction
+main() {
+    
+  // Creating List
+  List<int> init = [13, 2, -11, 142, -389, 32, 3032, 0];
+    
+  // Sorting List
+  init.sort();
+    
+  // Printing Sorted List
+  print(init); 
+}
